ebcffd6d1584979a02e83903631ae77b25a9a383
Dmitrii <dima12sik@yandex.ru>
2023-04-06 19:12:26 +0300

    updated tests — changed pathways in test_files

--- tests/test_db_operations.py
+++ tests/test_db_operations.py
@@ -1,24 +1,38 @@
 from utils.db_operations import operations_db
 import pytest
 import os.path
+import json
 
-PATH_file1 = os.path.join('tests_db_operation', 'file_test.json')
-PATH_file2 = os.path.join('tests_db_operation', 'file_for_test_operation.json')
+PATH_1 = os.path.join('.', 'file_test.json')
+PATH_2 = os.path.join('.', 'file_for_test_operation.json')
 
 
-@pytest.mark.parametrize("expected, way", [('Ошибка, файл не найден', os.path.join('utils', 'example.json')),
-                                           ('Проверьте целостность файла', 'file_test.json')])
+@pytest.fixture
+def get_filled_json_wrong():
+    with open(os.path.join('.', 'file_test.json'), 'w', encoding='utf-8') as db:
+        information = dict(zip(("a", "b"), (1, 2)))
+        db.write(json.dumps(information))
+
+
+@pytest.fixture
+def get_empty_json_file():
+    with open(os.path.join('.', 'file_test.json'), 'w', encoding='utf-8'):
+        pass
+
+
+@pytest.mark.parametrize("expected, way", [('Ошибка, файл не найден', 'example.json'),
+                                           ('Проверьте целостность файла', PATH_1)])
 def test_operations_db_validate_data(expected, way, get_empty_json_file):
     assert operations_db(way) == expected
 
 
 def test_operations_db_validate_json_data(get_filled_json_wrong):
     with pytest.raises(ValueError):
-        assert operations_db('file_test.json') == "Проверьте корректность данных в файле"
+        assert operations_db(PATH_1) == "Проверьте корректность данных в файле"
 
 
 def test_operations_db_sort():
-    db = operations_db('file_for_test_operation.json')
+    db = operations_db(PATH_2)
     data_sorted = []
     for elem in db:
         data_sorted.append(elem["date"])
@@ -26,10 +40,9 @@ def test_operations_db_sort():
 
 
 def test_operations_db_corrected_state():
-    db = operations_db('file_for_test_operation.json')
+    db = operations_db(PATH_2)
     data_corrected = []
     for elem in db:
         if elem['state'] == 'CANCELED':
             data_corrected.append(elem)
     assert data_corrected == []
-
